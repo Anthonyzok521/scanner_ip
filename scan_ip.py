fd952d1ab15fb7a41e7d7848f7121d415c657202
AC GAMES <66536793+Anthonyzok521@users.noreply.github.com>
2024-02-29 18:26:43 -0400

    Optimize Code

--- scan_ip.py
+++ scan_ip.py
@@ -1,7 +1,12 @@
-#Modules
-import nmap                                                                             import socket
+"""
+    SCANNER OF IPs
+
+"""
+from colorama import init, Fore, Style
+import socket
+import nmap
 import netifaces
-from colorama import *
+
 
 init() #Initialize colorama
 
@@ -11,14 +16,14 @@ sock.connect(('8.8.8.8', 0))
 
 local_ip = sock.getsockname()[0] #IP
 gateway = netifaces.gateways()[2][0][0] #Gateway
-red = gateway.split('.')[:3] #Network
-red = '.'.join(red) + '.0' #Format .0 end
+network = gateway.split('.')[:3] #Network
+network = '.'.join(network) + '.0' #Format .0 end
 sock.close()
 
 #Show IPs
-print(Style.BRIGHT + '-'*25)                                                            print(Fore.WHITE + "IP local: " + local_ip)
+print(Style.BRIGHT + '-'*25)
 print(Fore.GREEN + "Gateway: " + gateway)
-print(Fore.CYAN + "Red: " + red)
+print(Fore.CYAN + "network: " + network)
 print('-'*25)
 print(Style.NORMAL + Fore.WHITE)
 
@@ -37,21 +42,28 @@ else:
 
 
 ips = scanner.all_hosts()
-msg = "Addresses"
+MSG = "Addresses"
 print(Style.BRIGHT + Fore.GREEN)
 print('-'*25)
 print(f'{"|":<8}{msg}{"|":>8}')
 print('-'*25)
 
 #Table of divices scanners
-spc = 0
-def createSpaces(ip):
-    if len(ip) == 15:                                                                           return 3
-    else:
-        return abs(len(ip) -  15) + 3
+def create_spaces(address_):
+    """Create Spaces for separate the address
 
-for ip in ips:
-    spc = createSpaces(ip)
-    print("|", ip, sep=" "*5, end=f'{" "*spc}|\n')
+    Args:
+        address_ (list): _description_
+
+    Returns:
+        int: return the number of spaces
+    """
+    if len(address_) == 15:
+        return 3
+    else:
+        return abs(len(address_) -  15) + 3
+
+for address in ips:
+    spc = create_spaces(address)
+    print("|", address, sep=" "*5, end=f'{" "*spc}|\n')
 print('-'*25)
-~
